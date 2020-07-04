51f20cb6ab6aaae772a2eccefe1e5a763abec94c
Ljufin <willgertsch@gmail.com>
2020-07-03 17:31:18 -0700

    Implemented crt_3level_power. No support for covariates yet.

--- R/crt_continuous.R
+++ R/crt_continuous.R
@@ -9,7 +9,6 @@
 # R2: proportion of explained variation by covariate
 crt_2level_power <- function(n, J, delta, icc, alpha = 0.05, R2 = 0) {
 
-
   # NCP
   lam = (J * delta^2)/(4 * ((1 - R2) * icc + (1 - icc)/n))
 
@@ -24,3 +23,23 @@ crt_2level_power <- function(n, J, delta, icc, alpha = 0.05, R2 = 0) {
   return(power)
 }
 
+
+# Compute power for 3 level CRT
+# n: subjects per cluster
+# J: clusters per site
+# K: number of sites
+# delta: standardized effect size
+# rho_2: icc for level 2
+# rho_3: icc for level 3
+# alpha: significance level
+crt_3level_power <- function(n, J, K, delta, rho_2, rho_3, alpha = 0.05) {
+
+  # NCP
+  lam = (K * delta^2)/(4 * (rho_3 + (rho_2 + (1 - rho_2 - rho_3)/n)/J))
+
+  df2 = K-2
+
+  # power calculation
+  power = 1-pf(qf(1 - alpha,  1, df2), 1, df2, lam)
+  return(power)
+}
